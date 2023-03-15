aabae20070da7b9ea9c040c313bd09780ca694d0
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-03-15 10:51:37 +0100

    Quête 00 - Démarrer avec Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularQuest';
+  title = 'Bienvenue sur le site de Wendy ! ';
 }
